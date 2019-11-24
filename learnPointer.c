5efe7ff8678cfa1c4e88d70dacbb2263e35dc86c
motoyannn <techvo.1127@gmail.com>
2019-11-24 12:40:19 +0900

    Pointerについて学習するスクリプトの追加

---
+++ learnPointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    /*
+     * Pointer に関する参考
+     *
+     * 工学院大学 非情報学生のための C/C++ 入門
+     * https://brain.cc.kogakuin.ac.jp/~kanamaru/lecture/prog1/index.html
+     *
+     * メモリとアドレス / 配列と関数を用いた際のメモリ配置 / アドレスとポインタ / 配列とポインタ
+     */
+    return 0;
+}
